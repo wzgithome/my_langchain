00bc2e848321a14b6588c4b75058b1d9254aef3c
wangzhao <19966155901@163.com>
2026-06-10 23:15:35 +0800

    工具调用

--- llm/my_llm.py
+++ llm/my_llm.py
@@ -4,13 +4,13 @@ from langchain_openai import ChatOpenAI
 
 # 定义的模型类
 dotenv.load_dotenv(dotenv_path='../chapter02-model IO/.env')
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY4")
-os.environ["OPENAI_BASE_URL"] = os.getenv("OPENAI_BASE_URL4")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY2")
+os.environ["OPENAI_BASE_URL"] = os.getenv("OPENAI_BASE_URL2")
 
 
 # ================== 2. 初始化模型和工具 ==================
 model = ChatOpenAI(
-    model='glm-4.5-air',
+    model='qwen3.7-plus',
     temperature=0.6,
     # 开启深度思考
     # extra_body={"stream": True},
