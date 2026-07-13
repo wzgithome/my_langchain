f9e5f4762e07ca4bd81486dc8666cc5d9a6a6327
wangzhao <19966155901@163.com>
2026-07-13 12:22:41 +0800

    langchain中间件

--- llm/my_llm.py
+++ llm/my_llm.py
@@ -1,7 +1,7 @@
 import os
 import dotenv
 from langchain.chat_models import init_chat_model
-
+from langchain_community.chat_models.tongyi import ChatTongyi
 from langchain_openai import ChatOpenAI
 
 # 定义的模型类
@@ -16,12 +16,20 @@ model = init_chat_model(
     model='qwen3.6-flash-2026-04-16',
     temperature=0.6,
     model_provider='openai',
+    profile={"max_input_tokens": 128_000}
     # 开启深度思考
     # extra_body={"enable_thinking": True},
     # 启用联网搜索功能
     # extra_body={"enable_search": True},
 )
 
+model_tool = ChatOpenAI(
+    model="qwen3.6-flash-2026-04-16",
+    temperature=0.6,
+    # extra_body={"enable_thinking": False},
+
+    # max_tokens=128_000
+)
 
 
 
